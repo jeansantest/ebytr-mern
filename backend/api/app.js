40b6ffbfe567214ecccd5d7130b18bcf0886977e
Jean Esteves <jean.esteves2001@gmail.com>
2021-10-30 19:31:02 -0300

    fix: voltando ao padrao erro resolvido no atlas mongodb

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,15 +1,15 @@
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-// const todoRouter = require('../routes/todo');
-// const usersRouter = require('../routes/users');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const todoRouter = require('../routes/todo');
+const usersRouter = require('../routes/users');
 
-// const app = express();
+const app = express();
 
-// app.use(cors());
-// app.use(express.json());
-// app.get('/', (req, res) => res.send('Hello World!'));
-// app.use('/todo', todoRouter);
-// app.use('/users', usersRouter);
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/todo', todoRouter);
+app.use('/users', usersRouter);
 
-// module.exports = app;
+module.exports = app;
--- backend/api/server.js
+++ backend/api/server.js
@@ -1,16 +1,4 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const todoRouter = require('../routes/todo');
-const usersRouter = require('../routes/users');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.get('/', (req, res) => res.send('Hello World!'));
-app.use('/todo', todoRouter);
-app.use('/users', usersRouter);
+const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
