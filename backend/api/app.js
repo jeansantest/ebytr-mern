b5990bdddd6448f38735944d3f9997a90d329803
Jean Esteves <jean.esteves2001@gmail.com>
2021-10-30 12:18:32 -0300

    adiciona o cors e eslint ao projeeto

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Testando');
-  });
+  res.send('Testando');
+});
 
 module.exports = app;
