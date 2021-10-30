a20b3c0ae530fe26ed065397840bcca6a54a682c
Jean Esteves <jean.esteves2001@gmail.com>
2021-10-30 19:25:44 -0300

    heroku deploy

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,4 +1,16 @@
-const app = require('./app');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const todoRouter = require('../routes/todo');
+const usersRouter = require('../routes/users');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/todo', todoRouter);
+app.use('/users', usersRouter);
 
 const PORT = process.env.PORT || 5000;
 
