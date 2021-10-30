58d9aa9414ebd37f2602dec5fe04fe5a8290043a
Jean Esteves <jean.esteves2001@gmail.com>
2021-10-30 19:19:33 -0300

    testando deploy heroku

--- backend/api/app.js
+++ backend/api/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/todo', todoRouter);
 app.use('/users', usersRouter);
 
